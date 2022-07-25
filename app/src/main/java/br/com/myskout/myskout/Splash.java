23487442fdf35cc30ccc51af050e270c5ab0a645
DouglasBR25 <douglas2502@gmail.com>
2022-07-25 11:52:57 -0300

    Initial commit

---
+++ app/src/main/java/br/com/myskout/myskout/Splash.java
@@ -0,0 +1,13 @@
+package br.com.myskout.myskout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+    }
+}
