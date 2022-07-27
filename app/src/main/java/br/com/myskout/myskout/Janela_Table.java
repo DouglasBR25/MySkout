c5aedad9b7bab5a81af9a5f48cdddf20e4a054eb
DouglasBR25 <douglas2502@gmail.com>
2022-07-27 11:45:56 -0300

    Trabalhando com Layouts

---
+++ app/src/main/java/br/com/myskout/myskout/Janela_Table.java
@@ -0,0 +1,14 @@
+package br.com.myskout.myskout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Janela_Table extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.janela_table_layout);
+    }
+}
